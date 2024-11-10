97fa4abede694767d2f82cad50a62e3930a851a2
Vladimir Konrad <vladimirkonrad@gmail.com>
2024-11-10 16:37:52 +0100

    fp default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import {DatePicker} from "@nextui-org/date-picker";
+
 
 export default function Home() {
   return (
@@ -23,8 +23,7 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
-        <DatePicker />
-
+  
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
